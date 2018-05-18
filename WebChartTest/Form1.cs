b0e18c186a36b1b563b5b041c6e31976fd2274fa
koanse <ant.kondaurov@gmail.com>
2018-05-18 03:02:53 +0300

    Initial commit.

---
+++ WebChartTest/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebChartTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WebChart.Chart cc = new WebChart.ChartControl();
+            this.Controls.Add(cc);
+        }
+    }
+}
